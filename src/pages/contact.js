7fa401d0c0c702ea9d5523958ba3306c8b792a0c
Gramos <imeri.gramos@gmail.com>
2020-03-30 23:29:39 +0200

    form success

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react"
 import Layout from "../components/layout"
 import Logo from "../assets/designcode-logo-on-dark.svg"
-import axios from "axios"
+import { motion } from "framer-motion"
 // Style
 import "../styles/page-contact/contact.scss"
 
@@ -11,7 +11,39 @@ const encode = data => {
     .join("&")
 }
 
+const fadeUp = {
+  hidden: {
+    y: -100,
+    opacity: 0,
+  },
+  show: {
+    y: 0,
+    opacity: 1,
+  },
+}
+
+const fadeDown = {
+  hidden: {
+    y: 100,
+    opacity: 0,
+  },
+  show: {
+    y: 0,
+    opacity: 1,
+  },
+}
+
+const fade = {
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+  },
+}
+
 export default function Contact() {
+  const [appear, setAppear] = useState(false)
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -34,7 +66,8 @@ export default function Contact() {
       }),
     })
       .then(() => {
-        alert("success")
+        // alert("success")
+        setAppear(true)
       })
       .catch(() => {
         alert("error")
@@ -48,8 +81,17 @@ export default function Contact() {
       <div className="page-contact">
         <div className="container">
           <Logo />
-          <h2>Send me a message</h2>
-          <form
+          <motion.h2
+            variants={fadeUp}
+            initial="hidden"
+            animate={appear ? "hidden" : "show"}
+          >
+            Send me a message
+          </motion.h2>
+          <motion.form
+            variants={fade}
+            initial="hidden"
+            animate={appear ? "hidden" : "show"}
             name="contact"
             method="POST"
             data-netlify="true"
@@ -68,7 +110,7 @@ export default function Contact() {
             <input
               onChange={onChange}
               value={formData.email}
-              type="email"
+              // type="email"
               name="email"
               placeholder="Email"
               required
@@ -80,8 +122,24 @@ export default function Contact() {
               placeholder="Message"
               required
             />
-            <button type="submit">Send</button>
-          </form>
+            <motion.button
+              variants={fadeDown}
+              initial="hidden"
+              animate={appear ? "hidden" : "show"}
+              type="submit"
+            >
+              Send
+            </motion.button>
+          </motion.form>
+          <motion.div
+            variants={fadeUp}
+            initial="hidden"
+            animate={!appear ? "hidden" : "show"}
+            transition={{ delay: 0.5 }}
+            className="message-success"
+          >
+            <p>Thank you !</p>
+          </motion.div>
         </div>
       </div>
     </Layout>
