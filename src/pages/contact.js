29445f33d99d10d1be1ed57d3a51c9ab3277a7e3
Gramos <imeri.gramos@gmail.com>
2020-03-30 22:06:59 +0200

    form async

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,47 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Layout from "../components/layout"
 import Logo from "../assets/designcode-logo-on-dark.svg"
-
+import axios from "axios"
 // Style
 import "../styles/page-contact/contact.scss"
 
-export default function contact() {
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
+
+export default function Contact() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  })
+
+  const onChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  const onSubmit = e => {
+    e.preventDefault()
+    console.log(formData)
+    axios
+      .post("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encode({ "form-name": "contact", ...formData }),
+      })
+      .then(() => {
+        // console.log("response:", response);
+        alert("success")
+      })
+      .catch(() => {
+        alert("error")
+      })
+  }
+
+  useEffect(() => {}, [])
+
   return (
     <Layout>
       <div className="page-contact">
@@ -13,15 +49,36 @@ export default function contact() {
           <Logo />
           <h2>Send me a message</h2>
           <form
-            name="contact"
-            method="POST"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
+            onSubmit={onSubmit}
+            // name="contact"
+            // method="POST"
+            // data-netlify="true"
+            // data-netlify-honeypot="bot-field"
           >
             <input type="hidden" name="form-name" value="contact" />
-            <input type="text" name="name" placeholder="Name" required />
-            <input type="email" name="email" placeholder="Email" required />
-            <textarea name="message" placeholder="Message" required />
+            <input
+              onChange={onChange}
+              value={formData.name}
+              type="text"
+              name="name"
+              placeholder="Name"
+              required
+            />
+            <input
+              onChange={onChange}
+              value={formData.email}
+              type="email"
+              name="email"
+              placeholder="Email"
+              required
+            />
+            <textarea
+              onChange={onChange}
+              value={formData.message}
+              name="message"
+              placeholder="Message"
+              required
+            />
             <button type="submit">Send</button>
           </form>
         </div>
